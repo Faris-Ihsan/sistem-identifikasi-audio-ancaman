939e926d61aa73de19e787140858a180b5850f38
Faris-Ihsan <farisihsan32@yahoo.co.id>
2022-02-22 07:43:04 +0700

    Upload files feature

--- main/routes.py
+++ main/routes.py
@@ -1,19 +1,38 @@
 from main import app
 import os
-from flask import Flask, flash, request, redirect, url_for, render_template
+from flask import request, redirect, render_template
 from werkzeug.utils import secure_filename
-UPLOAD_FOLDER = '/main/static/audio'
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', name=os.getcwd())
 
+current_path = os.getcwd()
+app.config["AUDIO_UPLOADS"] = current_path + "\\main\\static\\audio\\upload\\"
+app.config["ALLOWED_AUDIO_EXTENSIONS"] = ["WAV"]
+
+def allowed_audio(filename):
+    if not "." in filename:
+        return False
+    ext = filename.rsplit(".", 1)[1]
+    if ext.upper() in app.config["ALLOWED_AUDIO_EXTENSIONS"]:
+        return True
+    else:
+        False
 
 @app.route('/uploader', methods = ['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        file = request.files['file']
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        return redirect(url_for('download_file', name=filename))
-    return "file uploded"
+        if request.files:
+            audio = request.files["audio"]
+            if audio.filename == "":
+                print ("Audio must have a filename")
+                return redirect(request.url)
+            if not allowed_audio(audio.filename):
+                print("harus wav brou")
+                return redirect(request.url)
+            else:
+                filename = secure_filename(audio.filename)
+                audio.save(os.path.join(app.config["AUDIO_UPLOADS"], filename))
+            return redirect(request.url)
+    return render_template("index.html")
